0e1f2ac54f942aaeb2e127ee03451a4b06512c85
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-11 01:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musician, instruments){
   var facts = [];
   for(var i = 0; i < musician.length; i++){
-    facts.push(`${musician[i]} plays ${instruments}`);
+    facts.push(`${musician[i]} plays ${instruments[i]}`);
   }
   return facts;
 }
